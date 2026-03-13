8a628fb12fded3f2c6a0f590ba3b83b2d2acaacc
DGWTH <Uthorn@digiwin.com>
2026-03-13 08:52:19 +0700

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbw6MttTUbpNco3GomMeuzlATYXz3r-IUPTWSMZuz07cDpXWBkZbFQTb5iEmpytcsJNB4Q/exec';
